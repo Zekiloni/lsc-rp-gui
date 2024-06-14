000dd307ce95ab3760d3c062fe7c18eb0bffb70e
Zekiloni <zekirija17@gmail.com>
2024-06-14 22:48:42 +0200

    home page cleanup, decoupled home page content into multiple components

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { HeroSectionComponent } from '../../components/home/hero-section/hero-section.component';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 import { FaqComponent } from '../../components/home/faq/faq.component';
+import { StartPlayingComponent } from '../../components/start-playing/start-playing.component';
 
 @Component({
   selector: 'app-home-page',
@@ -12,6 +14,8 @@ import { FaqComponent } from '../../components/home/faq/faq.component';
       ButtonModule,
       RippleModule,
       FaqComponent,
+      StartPlayingComponent,
+      AnimateOnScrollModule,
    ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
