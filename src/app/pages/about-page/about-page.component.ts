02f87743a6a6cd08944c7c84c44f8045109691ca
Zekiloni <zekirija17@gmail.com>
2024-06-14 22:50:58 +0200

    about page add lorem ipsum generated text content

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { HeroSectionComponent } from '../../components/home/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [],
+   imports: [
+      AnimateOnScrollModule,
+      HeroSectionComponent,
+   ],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
