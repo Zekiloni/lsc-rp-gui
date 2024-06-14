026644e74a93ea6da0d61875e69dae98ee9a5f90
Zekiloni <zekirija17@gmail.com>
2024-06-14 22:51:44 +0200

    start playing component completed

---
+++ src/app/components/start-playing/start-playing.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+
+@Component({
+  selector: 'app-start-playing',
+  standalone: true,
+   imports: [
+      ButtonModule,
+      RippleModule,
+   ],
+  templateUrl: './start-playing.component.html',
+  styleUrl: './start-playing.component.scss'
+})
+export class StartPlayingComponent {
+
+}
