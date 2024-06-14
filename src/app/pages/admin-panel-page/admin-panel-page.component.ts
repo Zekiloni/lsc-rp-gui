4e488822aef20cc821c0117b8f479629d92f806f
Zekiloni <zekirija17@gmail.com>
2024-06-14 22:51:33 +0200

    admin panel page initialized

---
+++ src/app/pages/admin-panel-page/admin-panel-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-panel-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-panel-page.component.html',
+  styleUrl: './admin-panel-page.component.scss'
+})
+export class AdminPanelPageComponent {
+
+}
