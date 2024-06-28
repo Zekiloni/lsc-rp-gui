f10a64ff5bf63c289c58fb6aeaa6319d5bbbe18e
Zekiloni <zekirija17@gmail.com>
2024-06-29 00:19:07 +0200

    select user account id from store (session), bans table view component, async pipe and tab view module, beautify import

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-import { CharactersCardsComponent } from '../../components/characters-cards/characters-cards.component';
+import { Component, Inject } from '@angular/core';
+import { CharactersCardsComponent } from '../../components/characters-cards';
+import { TabViewModule } from 'primeng/tabview';
+import { BansTableComponent } from '../../components/admin-record/bans-table';
+import { selectAccount } from '../../stores/account/account.selector';
+import { Store } from '@ngrx/store';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
    selector: 'app-dashboard-page',
    standalone: true,
    imports: [
       CharactersCardsComponent,
+      TabViewModule,
+      BansTableComponent,
+      AsyncPipe,
+      NgIf,
    ],
    templateUrl: './dashboard-page.component.html',
    styleUrl: './dashboard-page.component.scss',
 })
 export class DashboardPageComponent {
+   userAccount$ = this.store.select(selectAccount);
 
+   constructor(@Inject(Store) private store: Store) {
+   }
 }
