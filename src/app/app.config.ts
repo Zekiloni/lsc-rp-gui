a96f1286ac1b5921e5ed2ab0efa70af09b094c9c
Zekiloni <zekirija17@gmail.com>
2024-07-09 23:41:27 +0200

    Refactor, provide default environment in appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 
@@ -10,7 +10,7 @@ import { MessageService } from 'primeng/api';
 
 import { BASE_PATH } from './core/variables';
 import { routes } from './app.routes';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 import { accountReducer } from './stores/account/account.reducer';
 import { apiErrorInterceptor } from './core/interceptor/api-error.interceptor';
 import { authInterceptor } from './core/interceptor/auth.interceptor';
