bf66d57e572f1a1baadd4c53ceab63609a3522aa
Zekiloni <zekirija17@gmail.com>
2024-07-10 19:11:54 +0200

    home page, format, remove sa map component test

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,16 +1,16 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+
 import { HeroSectionComponent } from '../../components/home/hero-section';
 import { FaqComponent } from '../../components/home/faq/faq.component';
 import { StartPlayingComponent } from '../../components/start-playing';
 import { SaMapComponent } from '../../components/sa-map';
-import { RoleplayQuizComponent } from '../../components/roleplay-quiz';
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
+   selector: 'app-home-page',
+   standalone: true,
    imports: [
       HeroSectionComponent,
       ButtonModule,
@@ -19,15 +19,9 @@ import { RoleplayQuizComponent } from '../../components/roleplay-quiz';
       StartPlayingComponent,
       AnimateOnScrollModule,
       SaMapComponent,
-      RoleplayQuizComponent,
    ],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+   templateUrl: './home-page.component.html',
+   styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent implements AfterViewInit{
-   @ViewChild(SaMapComponent) saMapComponent!: SaMapComponent;
-
-   ngAfterViewInit(): void {
-      //this.saMapComponent.addToMap(0.0, 0.0);
-   }
+export class HomePageComponent {
 }
