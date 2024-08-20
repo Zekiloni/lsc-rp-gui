9327604cccc5a1dbbd6d83a5f998802fbd97a491
Zekiloni <zekirija17@gmail.com>
2024-08-20 02:42:22 +0200

    manage pending characters

--- src/app/pages/admin-panel-page/manage-pending-characters/manage-pending-characters.component.ts
+++ src/app/pages/admin-panel-page/manage-pending-characters/manage-pending-characters.component.ts
@@ -1,20 +1,26 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CharacterApiService } from '../../../core/api/characterApi.service';
 import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-manage-pending-characters',
-  standalone: true,
+   selector: 'app-manage-pending-characters',
+   standalone: true,
    imports: [
       NgIf,
       NgForOf,
       AsyncPipe,
+      TableModule,
+      ButtonModule,
    ],
    providers: [CharacterApiService],
-  templateUrl: './manage-pending-characters.component.html',
-  styleUrl: './manage-pending-characters.component.scss'
+   templateUrl: './manage-pending-characters.component.html',
+   styleUrl: './manage-pending-characters.component.scss',
 })
 export class ManagePendingCharactersComponent {
-   characterService = inject(CharacterApiService);
-   pendingCharacters = this.characterService.listUnapprovedCharacter();
+   $pendingCharacters = this.characterApiService.listUnapprovedCharacter();
+
+   constructor(private characterApiService: CharacterApiService) {
+   }
 }
