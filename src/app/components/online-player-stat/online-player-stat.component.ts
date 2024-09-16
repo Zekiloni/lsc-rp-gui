71c8735f8bc5fb5eed84e094a110c1a664fb9bf1
Zekiloni <zekirija17@gmail.com>
2024-09-16 20:28:23 +0200

    display online player stat graph

--- src/app/components/online-player-stat/online-player-stat.component.ts
+++ src/app/components/online-player-stat/online-player-stat.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { OnlinePlayerStatApiService } from '../../core/api/onlinePlayerStatApi.service';
 import { AsyncPipe, NgIf } from '@angular/common';
+import { ChartModule } from 'primeng/chart';
+import { OnlinePlayerStat } from '../../core/model/models';
+import { OnlinePlayerStatApiService } from '../../core/api/api';
 
 @Component({
    selector: 'app-online-player-stat',
@@ -8,6 +10,7 @@ import { AsyncPipe, NgIf } from '@angular/common';
    imports: [
       AsyncPipe,
       NgIf,
+      ChartModule,
    ],
    providers: [OnlinePlayerStatApiService],
    templateUrl: './online-player-stat.component.html',
@@ -17,5 +20,20 @@ export class OnlinePlayerStatComponent {
    $onlinePlayerStat = this.onlinePlayerStatApiService.listOnlinePlayersStat();
 
    constructor(private onlinePlayerStatApiService: OnlinePlayerStatApiService) {
+   }
+
+   transformToData(onlinePlayerStat: OnlinePlayerStat[]) {
+      return {
+         labels: onlinePlayerStat.map(data => new Date(data.createdAt).toLocaleTimeString()),
+         datasets: [
+            {
+               label: 'Player Count',
+               data: onlinePlayerStat.map(data => data.playerCount),
+               fill: false,
+               tension: 0.4,
+               borderColor: '#42A5F5',
+            },
+         ],
+      };
    }
 }
